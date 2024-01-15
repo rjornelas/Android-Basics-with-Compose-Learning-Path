0d88ec974682a313ec0207ca53aefa47bedf436c
rjornelas <rafael.j.ornelas@gmail.com>
2024-01-14 22:04:32 -0300

    Refactor with instructor solution

--- Compose-Article-App/app/src/main/java/com/rjornelas/compose_article_app/MainActivity.kt
+++ Compose-Article-App/app/src/main/java/com/rjornelas/compose_article_app/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,28 +35,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    ComposeArticleApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun ArticleImage(modifier: Modifier = Modifier){
-    val image = painterResource(id = R.drawable.bg_compose_background)
-    Image(painter = image,
-        contentDescription = null,
-        contentScale = ContentScale.Fit
-    )
-}
 
 @Composable
-fun ArticleText(headText: String, introText: String, bodyText: String, modifier: Modifier = Modifier){
-    Column(
-        horizontalAlignment = Alignment.Start,
-        modifier = modifier
-    ){
-        ArticleImage()
+fun ArticleText(imagePainter: Painter, headText: String, introText: String, bodyText: String, modifier: Modifier = Modifier){
+    Column(modifier = modifier){
+        Image(painter = imagePainter, contentDescription = null)
         Text(
             text = headText,
             fontSize = 24.sp,
@@ -76,8 +67,7 @@ fun ArticleText(headText: String, introText: String, bodyText: String, modifier:
             modifier = modifier
                 .padding(
                     start = 16.dp,
-                    end = 16.dp,
-                    bottom = 16.dp),
+                    end = 16.dp),
             textAlign = TextAlign.Justify
         )
     }
@@ -87,10 +77,16 @@ fun ArticleText(headText: String, introText: String, bodyText: String, modifier:
 @Composable
 fun ComposeArticlePreview() {
     ComposeArticleAppTheme {
-        ArticleText(
-            headText = stringResource(R.string.head_text),
-            introText = stringResource(R.string.intro_text),
-            bodyText = stringResource(R.string.body_text)
-        )
+        ComposeArticleApp()
     }
+}
+
+@Composable
+private fun ComposeArticleApp() {
+    ArticleText(
+        headText = stringResource(R.string.head_text),
+        introText = stringResource(R.string.intro_text),
+        bodyText = stringResource(R.string.body_text),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
 }
